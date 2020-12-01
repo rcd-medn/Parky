1887a700ff286f0bd7417654d1f665048fb76404
Ricardo Medina <para.sites.estudos@gmail.com>
2020-12-01 16:19:00 -0300

    Adicionados a interface INationalParkRepository e classe que a implementa NationalParkRepository.

---
+++ ParkyAPI/Repository/NationalParkRepository.cs
@@ -0,0 +1,51 @@
+﻿
+
+
+
+using ParkyAPI.Models;
+using ParkyAPI.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Repository
+{
+    public class NationalParkRepository : INationalParkRepository
+    {
+        public bool CreateNationalPark(NationalPark nationalPark)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CUpdateNationalPark(NationalPark nationalPark)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteNationalPark(NationalPark nationalPark)
+        {
+            throw new NotImplementedException();
+        }
+
+        public NationalPark GetNationalPark(int nationalParkId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<NationalPark> GetNationalParks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool NationalParkExist(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool NationalParkExist(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
