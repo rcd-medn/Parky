d52d241d74ae29595dba2576f73155fec320c01d
Ricardo Medina <para.sites.estudos@gmail.com>
2020-12-01 14:37:41 -0300

    Criadas a migration inicial do projeto e aplicada ao banco de dados.

--- ParkyAPI/Startup.cs
+++ ParkyAPI/Startup.cs
@@ -1,15 +1,14 @@
+
+
+
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ParkyAPI.Data;
 
 namespace ParkyAPI
 {
@@ -25,6 +24,7 @@ namespace ParkyAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ParkDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ParkyAPI")));
             services.AddControllers();
         }
 
