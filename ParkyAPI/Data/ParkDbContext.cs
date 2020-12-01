74a5c735c43e788222b040c0a721edb106adece9
Ricardo Medina <para.sites.estudos@gmail.com>
2020-12-01 10:23:14 -0300

    Adicionado o arquivo NationalPark (Domain Model). Criado o arquivo ParkDbContext e adicionada a string de conexão no arquivo appSetting.json

---
+++ ParkyAPI/Data/ParkDbContext.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+
+using Microsoft.EntityFrameworkCore;
+using ParkyAPI.Models;
+
+namespace ParkyAPI.Data
+{
+    public class ParkDbContext : DbContext
+    {
+        public ParkDbContext(DbContextOptions<ParkDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<NationalPark> NationalParks { get; set; }
+    }
+}
