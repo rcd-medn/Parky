0c0eb35aca32b9ab7273e93747ada28e3f6747d8
Ricardo Medina <para.sites.estudos@gmail.com>
2020-12-10 18:30:06 -0300

    Adicionada um classe de configuração com as URI da API a ser consumida.

---
+++ ParkyWeb/SD.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWeb
+{
+    public static class SD
+    {
+        public static string APIBaseUrl = "https://localhost:44396/";
+        public static string NationalParkAPIPath = APIBaseUrl + "api/v1/nationalParks";
+        public static string TrailAPIPath = APIBaseUrl + "api/v{version:apiVersion}/trails";
+    }
+}
