3957f560ab25cbedf61f2eb0c9e1be9361410725
Ricardo Medina <para.sites.estudos@gmail.com>
2020-12-14 18:07:17 -0300

    Necessário corrigir erro no formulário de criação de registro. O novo registro não está sendo gravado.

--- ParkyWeb/Controllers/NationalParksController.cs
+++ ParkyWeb/Controllers/NationalParksController.cs
@@ -7,6 +7,7 @@ using ParkyWeb.Models;
 using ParkyWeb.Repository.IRepository;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -28,6 +29,7 @@ namespace ParkyWeb.Controllers
             return Json(new { data = await _nationalParkRepository.GetAllAsync(SD.NationalParkAPIPath) });
         }
 
+        [HttpGet]
         public async Task<IActionResult> Upsert(int? id)
         {
             NationalPark obj = new NationalPark();
@@ -43,6 +45,49 @@ namespace ParkyWeb.Controllers
             }
 
             return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(NationalPark obj)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count > 0)
+                {
+                    byte[] p1 = null;
+                    using (var fs1 = files[0].OpenReadStream())
+                    {
+                        using (var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }
+                    }
+                    obj.Picture = p1;
+                }
+                else
+                {
+                    var objFromDb = await _nationalParkRepository.GetAsync(SD.NationalParkAPIPath, obj.Id);
+                    obj.Picture = objFromDb.Picture;
+                }
+
+                if (obj.Id == 0)
+                {
+                    await _nationalParkRepository.CreateAsync(SD.NationalParkAPIPath, obj);
+                }
+                else
+                {
+                    await _nationalParkRepository.UpdateAsync(SD.NationalParkAPIPath + obj.Id, obj);
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(obj);
+            }
         }
     }
 }
