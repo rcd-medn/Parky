a630040c89bb29e69bcf685659dfe113d0792e76
Ricardo Medina <para.sites.estudos@gmail.com>
2021-01-02 22:00:48 -0300

    Adicionado o controller UsersController com o método action Authenticate que executa uma chamada ao método Authenticate da classe UserRepository.

---
+++ ParkyAPI/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ParkyAPI.Models;
+using ParkyAPI.Repository.IRepository;
+
+namespace ParkyAPI.Controllers
+{
+    [Route("api/v{version:apiVersion}/Users")]
+    [ApiController]
+    [Authorize]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UsersController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpPost(Name = "authenticate")]
+        public IActionResult Authenticate([FromBody] User userModel)
+        {
+            var user = _userRepository.Authenticate(userModel.Username, userModel.Password);
+            if (user == null)
+            {
+                return BadRequest(new { message = "Usuário ou senha inválidos!" });
+            }
+
+            return Ok(user);
+        }
+    }
+}
