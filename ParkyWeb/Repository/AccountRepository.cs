534c96feb800da0f77be89e1e6931aadd5ff3836
Ricardo Medina <para.sites.estudos@gmail.com>
2021-01-06 17:48:57 -0300

    Adicionada a model User, a interface IAccountRepository e a classe de implementação AccountRepository.

---
+++ ParkyWeb/Repository/AccountRepository.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+
+
+using ParkyWeb.Models;
+using ParkyWeb.Repository.IRepository;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Repository
+{
+    public class AccountRepository : Repository<User>, IAccountRepository
+    {
+        private readonly IHttpClientFactory _httpClient;
+
+        public AccountRepository(IHttpClientFactory httpClient) : base(httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public Task<User> LoginAsync(string url, User objToCreate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RegisterAsync(string url, User objToRegister)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ParkyWeb/Repository/IRepository/IAccountRepository.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+
+
+using ParkyWeb.Models;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Repository.IRepository
+{
+    public interface IAccountRepository : IRepository<User>
+    {
+        Task<User> LoginAsync(string url, User objToCreate);
+        Task<bool> RegisterAsync(string url, User objToRegister);
+    }
+}
